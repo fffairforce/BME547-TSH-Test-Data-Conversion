9d481acf774bc81f82014eeaeefb9d6b0def790f
Your Name <you@example.com>
2019-02-14 18:53:40 -0500

    add local test_TSH-fix too long

--- TSH-test.py
+++ TSH-test.py
@@ -10,19 +10,20 @@ import diagnose func to test the function
 """
 
 
-@pytest.mark.parametrize("test_list, expected", [([2.0, 1.5, 3.1, 2.5,
-                                                   1.1, 3.0, 1.9, 0.3,
-                                                   0.7, 1.8, 2.2, 3.7],
-                                                  "Hyperthyroidism"),  # test the efficacy of diagnosis function
-                                                 ([2.8, 3.6, 3.3, 4.5,
-                                                  5.5, 5.0, 6.6, 3.5,
-                                                  2.7, 4.4, 4.1, 4.8],
-                                                  "Hyperthyroidism"),
-                                                 ([3.5, 1.2, 2.3, 2.8,
-                                                   2.9, 2.6, 3.5, 1.3],
-                                                  "normal thyroid function"),
-                                                 ([0.5], "Hyperthyroidism")  # test different length of list
-                                                 ])
+@pytest.mark.parametrize("test_list, expected",
+                         [([2.0, 1.5, 3.1, 2.5,
+                            1.1, 3.0, 1.9, 0.3,
+                            0.7, 1.8, 2.2, 3.7],
+                             "Hyperthyroidism"),  # test the efficacy of diagnosis function
+                          ([2.8, 3.6, 3.3, 4.5,
+                            5.5, 5.0, 6.6, 3.5,
+                            2.7, 4.4, 4.1, 4.8],
+                             "Hyperthyroidism"),
+                          ([3.5, 1.2, 2.3, 2.8,
+                            2.9, 2.6, 3.5, 1.3],
+                             "normal thyroid function"),
+                          ([0.5], "Hyperthyroidism")  # test different length of list
+                          ])
 def test_diagnosis_tsh(test_list, expected):
     from dictionary import diagnose
     answer = diagnose(test_list)
