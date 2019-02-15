16e238f39b304310252d47da0576bfae5d68d7d8
Your Name <you@example.com>
2019-02-14 21:25:08 -0500

    adjust test file name- fix list problem

--- test_dictionary.py
+++ test_dictionary.py
@@ -1,5 +1,4 @@
 import pytest
-
 """
 Just don't know how keep warning wrong pep8
 when putting right below def test_diagnosis_tsh(test_list, expected):
@@ -10,21 +9,23 @@ import diagnose func to test the function
 """
 
 
-@pytest.mark.parametrize("test_list, expected",
-                         [([2.0, 1.5, 3.1, 2.5,
-                            1.1, 3.0, 1.9, 0.3,
-                            0.7, 1.8, 2.2, 3.7],
-                             "Hyperthyroidism"),
-                          ([2.8, 3.6, 3.3, 4.5,
-                            5.5, 5.0, 6.6, 3.5,
-                            2.7, 4.4, 4.1, 4.8],
-                             "Hyperthyroidism"),
-                          ([3.5, 1.2, 2.3, 2.8,
-                            2.9, 2.6, 3.5, 1.3],
-                             "normal thyroid function"),
-                          ([0.5], "Hyperthyroidism")
-                          ])
-def test_diagnosis_tsh(test_list, expected):
+@pytest.mark.parametrize("a, expected", [
+
+    ("TSH,3.5,3.6,1.8,2.8,1.9,3.4,3,3.6,3,4", 'Normal thyroid function'),
+
+    ("TSH,2.7,1.4,2.5,3.1,0.4,1.8,3.1,3,3.8,0.9,2.3", 'Hyperthyroidism'),
+
+    ("TSH,6.3,6.7,6.3,7.6,2.1,6.9,7.1,4.1,7.2,3.5,2.9", 'Hypothyroidism'),
+
+])
+def test_diagnose_parametrize(a, expected):
+
+    """
+    The function of diagnose() is tested with three input conditons using
+    the paramertrize decorator.
+    Returns:
+        Passed if answer == expected
+    """
     from dictionary import diagnose
-    answer = diagnose(test_list)
-    assert answer == expected
+    answer = diagnose(a)
+    assert answer[1] == expected
