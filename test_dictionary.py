9a8bc3329c1b17b4eacb0b0da201d261efe8fb05
Your Name <you@example.com>
2019-02-14 21:28:40 -0500

    typo fix

--- test_dictionary.py
+++ test_dictionary.py
@@ -11,7 +11,7 @@ import diagnose func to test the function
 
 @pytest.mark.parametrize("a, expected", [
 
-    ("TSH,3.5,3.6,1.8,2.8,1.9,3.4,3,3.6,3,4", 'Normal thyroid function'),
+    ("TSH,3.5,3.6,1.8,2.8,1.9,3.4,3,3.6,3,4", 'normal thyroid function'),
 
     ("TSH,2.7,1.4,2.5,3.1,0.4,1.8,3.1,3,3.8,0.9,2.3", 'Hyperthyroidism'),
 
